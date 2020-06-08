3e6ecdf5031934942149849c0cb833278825a26b
Owen Goodwin <the404devs@gmail.com>
2020-06-08 11:31:16 -0400

    Gallery JS fix

--- js/app.js
+++ js/app.js
@@ -31,38 +31,6 @@ var showPanes = function(n) {
     tabs[n - 1].className += " active";
 }
 
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Dots controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("gallery-slide");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-
-
 window.onresize = function() {
     //to ensure we don't end up out of bounds when the window resizes itself
     var tabs = document.getElementsByClassName("tab");
