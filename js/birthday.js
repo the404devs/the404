a1704a9581ab1dfeac48035eb1399903b234b21e
Owen Bowden <the404devs@gmail.com>
2024-01-19 10:10:53 -0500

    revise birthday mode trigger so it actually works

--- js/birthday.js
+++ js/birthday.js
@@ -1,16 +1,28 @@
-function checkBdayCookie(name) {
-    let cook = name + "="
-    if (document.cookie.length > 0) {
-        os = document.cookie.indexOf(cook)
-        if (os == -1) {
-            // cookie set
-            document.location = "/birthday.html"
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return true;
         }
     }
-    // No cookies have been set - set it now.
-    document.cookie = name + "=beenHere"
+    return false;
 }
+
+function setCookie(cname){
+    const d = new Date();
+    d.setTime(d.getTime() + 31104000000);
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=true;" + expires + ";path=/";
+}
+
 let d = new Date();
-if (d.getMonth() == 0 && d.getDate() > 16 && d.getDate() < 20) {
-    checkBdayCookie("redirect");
+if (d.getMonth() == 0 && d.getDate() > 16 && d.getDate() < 20 && !getCookie("seenBirthday")) {
+    setCookie("seenBirthday");
+    location.href = "./birthday.html";
 }
