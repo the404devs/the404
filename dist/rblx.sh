11d815917fb6111a8d40b06bfc9efcbfd0f11c57
Owen Bowden (the404) <47672957+the404devs@users.noreply.github.com>
2025-06-16 11:53:52 -0400

    Update rblx.sh

--- dist/rblx.sh
+++ dist/rblx.sh
@@ -4,4 +4,4 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install flatpak -y
 sudo flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install flathub org.vinegarhq.Vinegar
+sudo flatpak install flathub org.vinegarhq.Vinegar -y
